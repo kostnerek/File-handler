0d45e4d99ce88667d9243e58f9bc7f82fe64d139
kostnerek <56977301+kostnerek@users.noreply.github.com>
2019-12-31 14:24:37 +0100

    Update text_control.h

--- text_control.h
+++ text_control.h
@@ -11,25 +11,25 @@ class text_control
         string fileName;
         string data;
         string all_text;
-        int a;
+        string a;
 
 
 
-        text_control();//(string file_name);
-
+        text_control();
+        text_control(string file_name);
         ~text_control();
 
 
-        int create_file(string file_name); //uzywamy w przypadku gdy nie mamy pliku (uzycie tego na istniejacym pliku usunie jego zawartosc
-        int open_file(string file_name);   //uzywamy w przypadku posiadania gotowego pliku
-        int save(string data);  //zapisuje do pliku wpisane przez nas dane
-        int print(); //wyswietla
-        int clear(); //czysci caly plik
-        int convert(string var);//konwersja z string na int potrzebna w wypadku odczytywania liczb z pliku
+        int create_file(string file_name); 
+        int open_file(string file_name);  
+        int save(string data);  
+        int print(); 
+        int clear(); 
+        int convert(string var);
+        int load_line(int line);
 
-
-        int load_all(); //wczytuje do zmiennej caly plik
-        int load_line(int line_number); // W tym miejscu mozna zmienic zeby wczytywac tekst do zmiennych, oraz wczytaj konkretna linie tekstu
+        int load_all(); 
+        
 
 
 
